964aefea75c4aef6eafd3a62b51b9ba01b1c8625
yafred <yafred@users.noreply.github.com>
2020-02-17 23:12:24 +0100

    create a zip file with generated code

--- routes/compile.js
+++ routes/compile.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const debug = require('debug')('asn1-playground:compile');
 const fs = require('fs');
+const archiver = require('archiver');
 const { spawn } = require('child_process');
 const rimraf = require("rimraf");
 
@@ -67,10 +68,10 @@ router.post('/', function (req, res, next) {
 	function compileJavaClasses() {
 		var args = ['-cp', process.env.ASN1_RUNTIME_JAR];
 
-		var dirEntries = fs.readdirSync(sessionPath, { withFileTypes : true });
+		var dirEntries = fs.readdirSync(sessionPath, { withFileTypes: true });
 		var packageName = '';
-		for(i = 0; i < dirEntries.length; i++) {
-			if(dirEntries[i].isDirectory()) {
+		for (i = 0; i < dirEntries.length; i++) {
+			if (dirEntries[i].isDirectory()) {
 				packageName = dirEntries[i].name;
 			}
 		}
@@ -121,9 +122,54 @@ router.post('/', function (req, res, next) {
 			consoleData.code = code.toString();
 			debug('Sending ' + JSON.stringify(consoleData).length + ' chars');
 			wsConnection.send(JSON.stringify(consoleData));
+
+			zipJavaClasses();
 		});
 	}
 
+	function zipJavaClasses() {
+		// create a file to stream archive data to.
+		var output = fs.createWriteStream(sessionPath + '.zip');
+		var archive = archiver('zip');
+
+		// listen for all archive data to be written
+		// 'close' event is fired only when a file descriptor is involved
+		output.on('close', function () {
+			debug(archive.pointer() + ' total bytes');
+			debug('archiver has been finalized and the output file descriptor has closed.');
+		});
+
+		// This event is fired when the data source is drained no matter what was the data source.
+		// It is not part of this library but rather from the NodeJS Stream API.
+		// @see: https://nodejs.org/api/stream.html#stream_event_end
+		output.on('end', function () {
+			debug('Data has been drained');
+		});
+
+		// good practice to catch warnings (ie stat failures and other non-blocking errors)
+		archive.on('warning', function (err) {
+			if (err.code === 'ENOENT') {
+				// log warning
+			} else {
+				// log  error
+			}
+		});
+
+		// good practice to catch this error explicitly
+		archive.on('error', function (err) {
+			// log error
+		});
+
+		// pipe archive data to the file
+		archive.pipe(output);
+
+		// append files from a glob pattern
+		archive.directory(sessionPath + '/', 'java');
+
+		// finalize the archive (ie we are done appending files but streams have to finish yet)
+		// 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+		archive.finalize();
+	}
 });
 
 module.exports = router;
